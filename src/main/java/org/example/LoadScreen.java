1395cedea0998bbab068457015cf5654d350e0a7
Klaytoh10 <klaytonh10@gmail.com>
2025-10-10 13:18:04 -0500

    input implemented

--- src/main/java/org/example/LoadScreen.java
+++ src/main/java/org/example/LoadScreen.java
@@ -6,23 +6,43 @@ import java.util.Scanner;
 public class LoadScreen {
 
 
-
     public void homeScreen(Scanner scanner) {
         System.out.println("""
                   ================================
                    ACCOUNTING LEDGER APPLICATION
                   ================================
-                  
+                
                   Options:
-                  
+                
                   D) Add Deposit
                   P) Make Payment (Debit)
                   L) Ledger
                   X) Exit
                 """);
 
-        String input = scanner.nextLine();
+        String input;
+        boolean isTrue = true;
 
+        while (true) {
+            input = scanner.nextLine();
+
+            switch (input) {
+                case "d":
+                    System.out.println("You selected: " + input);
+                    break;
+                case "p":
+                    System.out.println("You selected: " + input);
+                    break;
+                case "l":
+                    System.out.println("You selected: " + input);
+                    break;
+                case "x":
+                    System.out.println("You selected: " + input);
+                    break;
+                default:
+                    System.out.println(input + " is not an option...try again");
+            }
+        }
 
     }
 
