af14c729eac0aed2841294858ad466ddfeddc516
Klaytoh10 <klaytonh10@gmail.com>
2025-10-10 15:35:27 -0500

    set up Transaction class and beginning to set up array lists and file writting

--- src/main/java/org/example/LoadScreen.java
+++ src/main/java/org/example/LoadScreen.java
@@ -1,12 +1,22 @@
 package org.example;
 
+import javax.swing.text.DateFormatter;
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LoadScreen {
 
 
-    public void homeScreen(Scanner scanner) {
+
+    public void homeScreen(Scanner scanner) throws IOException {
         System.out.println("""
                   ================================
                    ACCOUNTING LEDGER APPLICATION
@@ -17,36 +27,86 @@ public class LoadScreen {
                   D) Add Deposit
                   P) Make Payment (Debit)
                   L) Ledger
-                  X) Exit
+                  X) Exit Program
+                  
                 """);
-
         String input;
-        boolean isTrue = true;
-
-        while (true) {
+        // loops until valid input is given
+        for (int i = 0; i == 0; i = i) {
             input = scanner.nextLine();
-
             switch (input) {
                 case "d":
-                    System.out.println("You selected: " + input);
+                    System.out.println("You selected: " + input.toUpperCase());
+                    i++;
+                    ledgerScreen(scanner);
                     break;
                 case "p":
-                    System.out.println("You selected: " + input);
+                    System.out.println("You selected: " + input.toUpperCase());
+                    i++;
+                    paymentScreen(scanner);
                     break;
                 case "l":
-                    System.out.println("You selected: " + input);
+                    System.out.println("You selected: " + input.toUpperCase());
+                    i++;
                     break;
                 case "x":
-                    System.out.println("You selected: " + input);
+                    System.out.println("You selected: " + input.toUpperCase());
+                    i++;
                     break;
                 default:
-                    System.out.println(input + " is not an option...try again");
+                    System.out.print(input + " is not an option...try again: ");
             }
         }
+    }
+
+    public void depositScreen(Scanner scanner) {
 
     }
 
-    public void ledgerScreen() {
+    public void paymentScreen(Scanner scanner) throws IOException {
+        System.out.println("""
+                
+                """);
+
+        ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        try(FileWriter writer = new FileWriter("src/main/resources/transactions.csv")) {
+            FileReader fileReader = new FileReader("src/main/resources/transactions.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            bufferedReader.readLine();
+
+            String input;
+
+            while((input = bufferedReader.readLine()) != null) {
+                String[] sections = input.split("\\|");
+            }
+        } catch(DateTimeException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void ledgerScreen(Scanner scanner) {
+
+        System.out.println("""
+                ==================================
+                              Ledger              
+                ==================================
+                
+                Display: 
+                
+                A) All Entries
+                D) Deposits Only
+                P) Payments Only
+                R) Custom Reports
+                
+                """);
+
+
+    }
+
+    public void exitProgram() {
 
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //Person person = new Person();
-        //System.out.println(person.toString());
-
         Scanner scanner = new Scanner(System.in);
 
         LoadScreen loadScreen = new LoadScreen();
 
         //Home Screen
-        loadScreen.homeScreen(scanner);
+        try {
+            loadScreen.homeScreen(scanner);
+        } catch(Exception e){
+            System.out.println(e);
+        }
 
 
     }
