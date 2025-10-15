2fe4bc415186783caafd6ff1cfe6ebbf33c6165e
Klayton <155929127+Klaytonh10@users.noreply.github.com>
2025-10-15 12:10:12 -0500

    Update transaction management and file handling
    Refactor transaction handling and file reading.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import javax.swing.text.DateFormatter;
 import java.io.*;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,20 +12,25 @@ public class Main {
     //Transactions list
     static ArrayList<Transaction> transactions = new ArrayList<Transaction>();
     static Scanner scanner = new Scanner(System.in);
-    static char userInput;
+    static String fileName = "src/main/resources/transactions.csv";
 
     public static void main(String[] args) throws IOException  {
 
-        FileReader fileReader = new FileReader("src/main/resources/transactions.csv");
+        FileReader fileReader = new FileReader(fileName);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
-        //transactions = loadTransactions();
-        mainMenu();
 
+        bufferedReader.readLine();
+
+        //loadTransactions();
+        mainMenu();
     }
 
     private static void mainMenu() {
-        userInput = ' ';
-        do {
+        //userInput = ' ';
+        boolean exit = false;
+
+        while (!exit) {
+
             System.out.println("""
                     =====================================
                         Accounting Ledger Application
@@ -33,49 +40,80 @@ public class Main {
                     P) Make Payment
                     L) Ledger
                     X) Exit
-                    
                     """);
             // get input
-            userInput = scanner.nextLine().charAt(0);
+            char userInput = Character.toUpperCase(scanner.nextLine().charAt(0));
 
             switch (Character.toUpperCase(userInput)) {
-                case 'D' -> { addDepositMenu(); break;}
-                case 'P' -> { makePayment(); break; }
+                case 'D' -> { addTransaction(true); break;} // add deposit
+                case 'P' -> { addTransaction(false); break; }
                 case 'L' -> { ledgerMenu(); break;}
+                case 'X' -> {exit = true; break;}
+                default -> {System.out.println("try again");}
             }
-        } while (Character.toUpperCase(userInput) != 'X');
+        };
     }
 
-    public static void makePayment() {
-    }
-
-    public static void addDepositMenu() {
+    public static void addTransaction(boolean isPositive) {
         // prompt user for the deposit information and save to csv
 
-        String date = LocalDate.now().toString();
-        System.out.print("Enter Current Time: ");
-        String time = scanner.nextLine();
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd|HH:mm:ss");
+        String timeDate = localDateTime.format(formatter);
+        String[] timeDateParts = timeDate.split("\\|");
+        String date = timeDateParts[0];
+        String time = timeDateParts[1];
+
+
         System.out.print("Enter Deposit Description: ");
         String description = scanner.nextLine();
         System.out.print("Enter Vendor Name: ");
         String vendor = scanner.nextLine();
         System.out.print("Enter Deposit Amount: $");
-        String amount = scanner.nextLine();
-        String[] data = {date, time, description, vendor, amount};
-        Transaction transaction = new Transaction(data[0], data[1], data[2], data[3], data[4]);
+        double amount = scanner.nextDouble();
+        if(!isPositive) {
+            amount = amount * -1;
+        }
+        Transaction transaction = new Transaction(date, time, description, vendor, amount);
         transactions.add(transaction);
 
-        System.out.println(transaction + " added");
+        //write transactions to file
+        try(FileWriter fileWriter = new FileWriter(fileName, true)){
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            String tranString = date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
+            bufferedWriter.write(tranString);
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 
     public static void ledgerMenu() {
         //display ledger screen
     }
 
-    public static void loadTransactions() {
-        try(FileWriter fileWriter = new FileWriter("src/main/resources/transactions.csv")) {
+    // get all negative transactions
+    public static void getPayments() {
 
+        for (int i = 0; i < transactions.size(); i++) {
+            if (transactions.get(i).getAmount() < 0) {
+                System.out.println();
+            }
 
         }
     }
+
+    //public static void searchVendor() {
+    //
+    //    for(int i = 0; i < transactions.size(); i++) {
+    //        if(transactions.get(i).getVendor().equalsIgnoreCase()) {
+    //
+    //        }
+    //    }
+    //}
+
+
 }
